06c0af4ac760953199a5112f60c26444e4569534
Deepak Rana <111626727+DeepakRana07@users.noreply.github.com>
2024-03-30 01:40:13 +0530

    tailwind project
    tailwind project added

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"]
+      },
+
+      colors:{
+        deepblue:"#02042a",
+        lightblue:"#2b84ea",
+        lightblue300:"#4b94ed",
+        lightblue500:"#0b72e7"
+      }
+    },
+  },
+  plugins: [],
+}
+
